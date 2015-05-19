26deefff07093bc02f7f458bf4231f3f7952cb1e
Saphali <Saphali@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-19 07:25:16 +0000

    email покупателя на Робокассу
    git-svn-id: https://plugins.svn.wordpress.org/robokassa-payment-gateway-saphali/trunk@1163128 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wc-robokassa.php
+++ wc-robokassa.php
@@ -3,7 +3,7 @@
   Plugin Name: Robokassa Payment Gateway (saphali)
   Plugin URI: 
   Description: Allows you to use Robokassa payment gateway with the WooCommerce plugin.
-  Version: 1.0.1
+  Version: 1.0.2
   Author: Alexander Kurganov, Saphali
   Author URI: http://saphali.com
  */
@@ -124,6 +124,14 @@ class WC_ROBOKASSA extends WC_Payment_Gateway{
   * @return void
   */
 	function init_form_fields(){
+		$debug = __('Включить логирование (<code>woocommerce/logs/' . $this->id . '.txt</code>)', 'woocommerce');
+		if ( !version_compare( WOOCOMMERCE_VERSION, '2.0', '<' ) ) {
+			if ( version_compare( WOOCOMMERCE_VERSION, '2.2.0', '<' ) )
+			$debug = str_replace( $this->id, $this->id . '-' . sanitize_file_name( wp_hash( $this->id ) ), $debug );
+			elseif( function_exists('wc_get_log_file_path') ) {
+				$debug = str_replace( 'woocommerce/logs/' . $this->id . '.txt', wc_get_log_file_path( $this->id ) , $debug );
+			}
+		}
 		$this->form_fields = array(
 				'enabled' => array(
 					'title' => __('Включить/Выключить', 'woocommerce'),
@@ -165,7 +173,7 @@ class WC_ROBOKASSA extends WC_Payment_Gateway{
 				'debug' => array(
 					'title' => __('Debug', 'woocommerce'),
 					'type' => 'checkbox',
-					'label' => __('Включить логирование (<code>woocommerce/logs/paypal.txt</code>)', 'woocommerce'),
+					'label' => $debug,
 					'default' => 'no'
 				),
 				'description' => array(
@@ -230,7 +238,11 @@ class WC_ROBOKASSA extends WC_Payment_Gateway{
 				'InvId' => $order_id,
 				'SignatureValue' => md5($crc),
 				'Culture' => $this->lang,
+				//'Encoding' => 'utf-8',
 			);
+		if(!empty($order->billing_email)) {
+			$args['Email'] = $order->billing_email;
+		}
 		if(!empty($this->outsumcurrency)) {
 			$args['OutSumCurrency'] = $this->outsumcurrency;
 		}
