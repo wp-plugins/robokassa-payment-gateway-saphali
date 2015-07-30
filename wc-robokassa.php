b1bdefce87a50eec67f5970ccd46613056e5d7d8
Saphali <Saphali@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-30 05:52:03 +0000

    readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/robokassa-payment-gateway-saphali/trunk@1209444 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wc-robokassa.php
+++ wc-robokassa.php
@@ -269,7 +269,7 @@ class WC_ROBOKASSA extends WC_Payment_Gateway{
 
 		return array(
 			'result' => 'success',
-			'redirect'	=> add_query_arg('order', $order->id, add_query_arg('key', $order->order_key, get_permalink(woocommerce_get_page_id('pay'))))
+			'redirect'	=> add_query_arg('order-pay', $order->id, add_query_arg('key', $order->order_key, get_permalink(woocommerce_get_page_id('pay'))))
 		);
 	}
 	
